02d1707f5bcc02e9691e99239f09aa0127264b00
John Swindin <johnswindin@gmail.com>
2018-03-14 03:50:54 +0000

    How to use Box

--- sandbox/myBox.js
+++ sandbox/myBox.js
@@ -1,13 +1,22 @@
+const map = fn => coll => Array.prototype.map.call(coll, fn)
+const reduce = (fn, init) => coll => Array.prototype.reduce.call(coll, fn, init)
+
+const double = x => x * 2
+const sum = (p, c) => p + c
+
+const mapDouble = map(double)
+const sumAllNums = reduce(sum, 0)
+
 const Box = x => ({
   do: f => Box(f(x)),
   fold: f => f(x),
   inspect: () => `Box(${x})`
 })
 
-console.log('more ...')
+console.log('Box use case ...')
 
 console.log(
   Box([1,2,3,4,5])
-    .do(ary => ary.map(x => x * 2))
-    .fold(x => x)
+    .do(ary => mapDouble(ary))
+    .fold(arr => sumAllNums(arr))
 )
