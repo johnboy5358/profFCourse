9810f3e0a32788a79b236aa4a6d2e5d540029f1d
John Swindin <johnswindin@gmail.com>
2018-03-17 16:03:30 +0000

    Added new file - Another example of using Box.

---
+++ sandbox/highestNumberInCollection[using Box].js
@@ -0,0 +1,26 @@
+const Box = x => ({
+  map: f => Box(f(x)),
+  fold: f => f(x),
+  inspect: () => `Box(${x})`
+})
+
+const map = fn => xs => Array.prototype.map.call(xs, fn)
+const splitOn = splitBy => string => String.prototype.split.call(string, splitBy)
+const mathMax = aryNums => Math.max.apply(null, aryNums)
+
+const splitOnCommaSpace = splitOn(', ')
+const maxNumbers = mathMax(splitOnCommaSpace)
+
+function highestNumber(stringNumbers) {
+  return Box(stringNumbers)
+    .map(splitOnCommaSpace)
+    .map(ary => map(Number)(ary))
+    .fold(ary => Math.max.apply(null, ary))
+}
+
+
+const numbers = "7, 5, 500, -8, 0, 3, 56, -1000"
+const result = highestNumber(numbers)
+
+console.log(result)
+console.log((result === 500 ? "Test passed" : "Test failed"))
