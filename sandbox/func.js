06e6b9bd8556ef700cfdec9b2a583b22c21d096c
johnboy5358 <johnswindin@gmail.com>
2018-03-18 10:37:02 +0000

    Added function tryCatch to func.js.

--- sandbox/func.js
+++ sandbox/func.js
@@ -7,16 +7,26 @@ const Box = x => ({
 
 // Either composable branching 
 const Right = x => ({
+  chain: f => f(x),
   map: f => Right(f(x)),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`
 })
 
 const Left = x => ({
+  chain: f => Left(x),
   map: f => Left(x),
   fold: (f, g) => f(x),
   inspect: () => `Left(${x})`
 })
+
+const tryCatch = f => {
+  try {
+    return Right(f())
+  } catch (e) {
+    return Left(e)
+  }
+}
 
 // General functions
 const identity = x => x
