4920282a6cff232db3578e659eb3a2931a70b945
johnboy5358 <johnswindin@gmail.com>
2018-03-21 10:17:44 +0000

    Changes to percentageDiscount.js

--- one-linear-data-flow/percentageDiscount.js
+++ one-linear-data-flow/percentageDiscount.js
@@ -1,6 +1,9 @@
 const replace = (regex, _with) => str => String.prototype.replace.call(str, regex, _with)
 const replace$withNullString = replace('$', '')
 const replacePercentWithNullString = replace(/\%/g, '')
+const percent = pcent => num => num * pcent / 100
+const takeOnePercent = percent(1)
+const identity = x => x
 
 const Box = x => ({
   map: f => Box(f(x)),
@@ -11,23 +14,28 @@ const Box = x => ({
 const moneyToFloat = str => {
   return Box(str)
     .map(replace$withNullString)
-    .fold(r => parseFloat(r))
+    .fold(parseFloat)
 }
 
 const percentToFloat = str => {
   return Box(str)
     .map(replacePercentWithNullString)
-    .fold(r => parseFloat(r * 0.01))
+    .fold(takeOnePercent)
 }
 
 const applyDiscount = (price, discount) => {
   const cost = Box(price)
     .map(moneyToFloat)
-    .fold(x=>x)
+    .fold(identity)
   return `$${cost - cost * percentToFloat(discount)}`
 }
 
-console.log(applyDiscount("$15", "20%"))
+console.log((applyDiscount("$15", "20%") === '$12') ? 'Test OK' : 'Test Failed!')
+// => Test OK
+console.log((applyDiscount("$100", "10%") === '$90') ? 'Test OK' : 'Test Failed!')
+// => Test OK
+console.log((applyDiscount("$100", "10%") === '$50') ? 'Test OK' : 'Test Failed!')
+// => Test Failed!
 
 /*
   * The old way...
